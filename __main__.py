83f9df210010a9fdf1e96f232719282b4a356ba6
Elton Santos <seltons1@gmail.com>
2023-07-20 14:26:14 -0300

    Change data transform in a separated class.

--- __main__.py
+++ __main__.py
@@ -1,7 +1,8 @@
 # %%
 from pyspark.sql import SparkSession
 from delta import *
-from pyspark.sql.functions import split, regexp_replace
+from format_file import FormatInDataFrame
+
 
 
 if __name__ == '__main__':
@@ -14,27 +15,13 @@ if __name__ == '__main__':
     spark = configure_spark_with_delta_pip(builder).getOrCreate()
 
     # Read and transform dataframe
-    data = spark.read.text("files/", lineSep="\n")
-    df_colunas = data.select(split(data['value'], '\t').alias('colunas'))
-    df_final = df_colunas
-    
-    # Explode one column in various columns
-    for i in range(7):
-        df_final = df_final.withColumn(f"coluna{i+1}", regexp_replace(df_colunas['colunas'][i],'\r',''))
-    
-    # Remove first row, that conteins old columns names
-    df_final = df_final.filter(df_final["coluna1"] != "No")
-    
-    # Drop old column
-    df_final = df_final.drop(df_final["colunas"])
-    
-    # Drop another unused columns
-    for i in [1,2,3,5,6]:
-        df_final = df_final.drop(df_final[f"coluna{i}"])
+    path = "files"
+    obj = FormatInDataFrame(path, spark)
+    df = obj.format()
 
     # Show dataframe
-    df_final.show(truncate=False)
+    df.show(truncate=False)
 
     # Show dataframe schema
-    df_final.printSchema()
+    df.printSchema()
 
