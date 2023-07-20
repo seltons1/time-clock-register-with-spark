9c0afb0f66ff6c4270c00c57bf6da47b6c1d9310
Elton Santos <seltons1@gmail.com>
2023-07-20 09:02:32 -0300

    Finishing file transformer

--- __main__.py
+++ __main__.py
@@ -17,6 +17,24 @@ if __name__ == '__main__':
     data = spark.read.text("files/", lineSep="\n")
     df_colunas = data.select(split(data['value'], '\t').alias('colunas'))
     df_final = df_colunas
+    
+    # Explode one column in various columns
     for i in range(7):
         df_final = df_final.withColumn(f"coluna{i+1}", regexp_replace(df_colunas['colunas'][i],'\r',''))
+    
+    # Remove first row, that conteins old columns names
+    df_final = df_final.filter(df_final["coluna1"] != "No")
+    
+    # Drop old column
+    df_final = df_final.drop(df_final["colunas"])
+    
+    # Drop another unused columns
+    for i in [1,2,3,5,6]:
+        df_final = df_final.drop(df_final[f"coluna{i}"])
+
+    # Show dataframe
     df_final.show(truncate=False)
+
+    # Show dataframe schema
+    df_final.printSchema()
+
