a51374e4ab6359be45af79f6b43115a11d341708
Elton Santos <seltons1@gmail.com>
2023-07-24 11:06:01 -0300

    Creating method to customize the dataframe.

--- __main__.py
+++ __main__.py
@@ -2,6 +2,10 @@
 from pyspark.sql import SparkSession
 from delta import *
 from format_file import FormatInDataFrame
+from datetime import datetime
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+
 
 
 
@@ -19,8 +23,55 @@ if __name__ == '__main__':
     obj = FormatInDataFrame(path, spark)
     df = obj.format()
 
+    colunas = df.columns
+    df_final = None
+    linhas_list = []
+    for coluna in colunas:
+        if coluna != "name":
+
+            # Explode a lista em elementos individuais
+            df_exploded = df.select("name", coluna).withColumn("elemento", explode(coluna))
+            df1_exploded = df.select("name", coluna)
+
+            col_name_elem = df_exploded.select("elemento", "name").collect()
+        
+            linhas = df.select("name", coluna).collect()
+
+            for linha in linhas:
+                #print(linha.__getitem__("name"))
+                lista_horarios = linha.__getitem__(coluna)
+                #print(lista_horarios)
+                index = 0
+                entrada_manha = datetime.strptime('00:00:00', "%H:%M:%S").time()
+                saida_manha = datetime.strptime('00:00:00', "%H:%M:%S").time()
+                entrada_tarde = datetime.strptime('00:00:00', "%H:%M:%S").time()
+                saida_tarde = datetime.strptime('00:00:00', "%H:%M:%S").time()
+            
+                for horario in lista_horarios:
+                    
+                    horario_time = datetime.strptime(horario, "%H:%M:%S").time()
+                    # Entrada manhã
+                    if index == 0:
+                        entrada_manha = horario
+                    # Saída manhã
+                    if index == 1:
+                        saida_manha = horario
+                    # Entrada tarde
+                    if index == 2:
+                        entrada_tarde = horario
+                    # Saída manhã
+                    if index == 3:
+                        saida_tarde = horario
+                    
+                    index += 1
+
+                data = datetime.strptime(coluna, "%Y/%m/%d").date()
+                linhas_list.append(Row(name=str(linha.__getitem__("name")), data=data, entrada_manha=entrada_manha, saida_manha=saida_manha, entrada_tarde=entrada_tarde, saida_tarde=saida_tarde ))
+    
+    df = obj.create_dataframe_with_column(linhas_list)
+
     # Show dataframe
-    df.show(truncate=False)
+    df.show(30,truncate=False)
 
     # Show dataframe schema
     df.printSchema()
--- format_file.py
+++ format_file.py
@@ -1,6 +1,6 @@
 
-from pyspark.sql.functions import split, regexp_replace
-
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
 class FormatInDataFrame():
 
     def __init__(self, path, spark):
@@ -29,11 +29,39 @@ class FormatInDataFrame():
 
         # Change column names to new names
         df_final = df_final.withColumnRenamed("col4","name")
-        df_final = df_final.withColumnRenamed("col7","datetime")
 
-        df_final = df_final.withColumn("time", split(df_final["datetime"]," ").getItem(2))
+        df_final = df_final.withColumn("time", split(df_final["col7"]," ").getItem(2))
+        df_final = df_final.withColumn("day", split(df_final["col7"]," ").getItem(0))
 
+        df_final = df_final.drop(df_final["col7"])
 
+        # Pivotando as linhas em colunas por data
+        df_pivot = df_final.groupBy("name").pivot("day").agg(collect_list("time"))
+        df_pivot = df_pivot.drop('null')
+    
 
-        return df_final
+        return df_pivot
 
+    def create_dataframe_with_column(self, linhas):
+
+        # Crie um DataFrame inicial
+        schema = StructType([StructField("name", StringType(), nullable=False),
+                            StructField("data", DateType(), nullable=False),
+                            StructField("entrada_manha", StringType()),
+                            StructField("saida_manha", StringType()),
+                            StructField("entrada_tarde", StringType()),
+                            StructField("saida_tarde", StringType())])
+        
+        df_final = self.spark.createDataFrame(linhas, schema)
+        df_final = df_final.withColumn("arrived_morning", to_timestamp(df_final.entrada_manha))
+        df_final = df_final.withColumn("leave_morning", to_timestamp(df_final.saida_manha))
+        df_final = df_final.withColumn("arrived_afternoon", to_timestamp(df_final.entrada_tarde))
+        df_final = df_final.withColumn("leave_afternoon", to_timestamp(df_final.saida_tarde))
+        df_final = df_final.withColumn("week_day", date_format(df_final.data, "EEEE"))
+
+        df_final.createOrReplaceTempView("REG")
+        df = self.spark.sql("select name, data, arrived_morning, leave_morning, arrived_afternoon, leave_afternoon, \
+                leave_morning-arrived_morning as morning_work, leave_afternoon-arrived_afternoon as afternoon_work, \
+                morning_work+afternoon_work as total_work, arrived_afternoon-leave_morning as lunch_time, week_day from REG")
+
+        return df
